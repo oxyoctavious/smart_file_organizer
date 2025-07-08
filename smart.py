d396a83984a0d2fe39df02c417ffdd32ea4ec79e
Your GitHub Username <vedantbvidhate@gmail.com>
2025-07-08 15:25:07 +0530

    Completed the project

--- smart.py
+++ smart.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 def list_files(directory):
     """Found file: horse1.jpeg
@@ -6,7 +7,9 @@ def list_files(directory):
     Found file: human2.jpeg
     Found file: LT E-Bill.pdf
     Found file: vedant.txt
-    Found file: zebra.jpeg"""     
+    Found file: zebra.jpeg"""   
+    
+               
     
     def get_folder_for_file(filename):
         """
@@ -17,7 +20,7 @@ def list_files(directory):
         extension = os.path.splitext(filename)[1].lower()
     
         FILE_TYPE_MAP= {
-            "Images": [".jpg",".jepg",".png",".gif"],
+            "Images": [".jpg",".jpeg",".png",".gif"],
             "Documents": [".pdf",".docx",".txt",".xlsx"],
             "Videos": [".mp4",".mov",".avi",".mkv"],
             "Music": [".mp3",".wav",".aac"],
@@ -30,6 +33,24 @@ def list_files(directory):
                 return folder_name
         
         return "others"
+    
+    for filename in os.listdir(directory):
+        file_path = os.path .join(directory, filename)
+        
+        if os.path.isfile(file_path):
+            target_folder= get_folder_for_file(filename)
+            target_folder_path= os.path.join(directory, target_folder)
+            
+            #Creat folder if it does not exist
+            
+            os.makedirs(target_folder_path, exist_ok=True)
+            
+            #Builds full destination path
+            destination_path= os.path.join(target_folder_path, filename)
+            
+            #Moves the file in
+            shutil.move(file_path, destination_path) 
+            print(f"Moved {filename}--> {target_folder}")  
     
     
     
